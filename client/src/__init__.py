78a8a3bba987e66576416c52a531e9de459490e8
Aidan Harvey-Nelson <firzen2550@gmail.com>
2024-08-26 12:37:50 +1000

    Added Game State function parsing

--- client/src/__init__.py
+++ client/src/__init__.py
@@ -1,2 +1,3 @@
-# from .game_state import GameState
+# ruff: noqa
+from .game_state import GameState, Player, Location, Turns, Card, Hand
 from .log_parser import LogParser
--- client/src/log_parser.py
+++ client/src/log_parser.py
@@ -10,65 +10,71 @@ class LogParser:
         """Initialise class using log file name."""
         self.file_name = file_name
         self.parent_types = [
-            r'CubeGame.Location, SecondDinner.CubeGame.Logic',
-            r'CubeGame.Player, SecondDinner.CubeGame.Logic'
+            r"CubeGame.Location, SecondDinner.CubeGame.Logic",
+            r"CubeGame.Player, SecondDinner.CubeGame.Logic",
         ]
         self.child_types = [
-            r'CubeGame.CardStaged, SecondDinner.CubeGame.Logic'
-            r'CubeGame.Card, SecondDinner.CubeGame.Logic',
+            r"CubeGame.CardStaged, SecondDinner.CubeGame.Logic" r"CubeGame.Card, SecondDinner.CubeGame.Logic",
         ]
+        self.hand_type = r"CubeGame.Hand, SecondDinner.CubeGame.Logic"
 
-    def extract_state(self, game_type='remote'):
+    def extract_state(self, game_type="remote"):
         """Extracts state information into JSON document.
 
         Used to extract the actual objects and removes refs
         """
-        with open(self.file_name, 'r', encoding='utf-8-sig') as file:
+        with open(self.file_name, "r", encoding="utf-8-sig") as file:
             state_file = json.load(file)
-            if game_type == 'remote':
-                return self.extract_remote_game(state_file['RemoteGame'])
+            if game_type == "remote":
+                return self.extract_remote_game(state_file["RemoteGame"])
             else:
-                raise NotImplementedError('Non remote game state is not curently implemented')
+                raise NotImplementedError("Non remote game state is not curently implemented")
 
-    def extract_remote_game(self, remote_game_json:dict) -> dict:
+    def extract_remote_game(self, remote_game_json: dict) -> dict:
         """Extacts state from the remote game section."""
         final_state = {}
         # Extract State Information
-        if not remote_game_json['ClientPlayerInfo']['CardsDrawn']:
+        if not remote_game_json["ClientPlayerInfo"]["CardsDrawn"]:
             return final_state
-        game_state = remote_game_json['GameState']
+        game_state = remote_game_json["GameState"]
 
-        final_state['state'] = {
-            'current_turn': game_state['Turn'],
-            'current_cubes': game_state['CubeValue'],
-            'next_turn_cubes': game_state['CubeValueNextTurn']
+        final_state["state"] = {
+            "current_turn": game_state["Turn"],
+            "current_cubes": game_state["CubeValue"],
+            "next_turn_cubes": game_state["CubeValueNextTurn"],
         }
-        if 'ClientResultMessage' in game_state:
+        if "ClientResultMessage" in game_state:
             # Assumption is first result is player needs to be validated
-            final_state['result'] = {
-                'winner': game_state['ClientResultMessage']['GameResultAccountItems'][0]['IsWinner'],
-                'cubes_traded': game_state['ClientResultMessage']['FinalCubeValue']
+            final_state["result"] = {
+                "winner": game_state["ClientResultMessage"]["GameResultAccountItems"][0]["IsWinner"],
+                "cubes_traded": game_state["ClientResultMessage"]["FinalCubeValue"],
             }
-
 
         # Remove Pre Start turn so we have a single view
         # Extract Location Information
-        game_state.pop('GameAtPreStartTurn')
-        for extract in self.extract_ids(game_state):
-            if 'Player' in extract[r'$type']:
-                player_id = 1 if game_state['Players'][0][r'$id'] == extract[r'$id'] else 2
-                final_state[f'player_{player_id}'] = {
-                    'hand': extract['Hand'],
-                    'deck': extract['Deck'],
-                    'current_energy': extract.get('CurrentEnergy', extract.get('MaxEnergy', None)),
-                    'has_snapped': None # Need to find how this is stored
+        game_state.pop("GameAtPreStartTurn")
+        for extract in self.extract_parent_ids(game_state):
+            if "Player" in extract[r"$type"]:
+                if game_state["Players"][0][r"$id"] == extract[r"$id"]:
+                    player_id = 1
+                else:
+                    player_id = 2
+                final_state[f"player_{player_id}"] = {
+                    "hand": extract["Hand"]
+                    if r"$id" in extract["Hand"]
+                    else self.extract_specific_ids(game_state, extract["Hand"][r"$ref"], self.hand_type),
+                    "deck": extract["Deck"]
+                    if r"$id" in extract["Deck"]
+                    else self.extract_specific_ids(game_state, extract["Deck"][r"$ref"], self.hand_type),
+                    "current_energy": extract.get("CurrentEnergy", extract.get("MaxEnergy", None)),
+                    "has_snapped": None,  # Need to find how this is stored
                 }
-            elif 'Location' in extract[r'$type']:
-                if 'location' not in final_state:
-                    final_state['location'] = {}
-                final_state['location'][extract.get('SlotIndex', 3)] = {
-                    'player_1_cards': extract['Player1Cards'],
-                    'player_2_cards': extract['Player2Cards']
+            elif "Location" in extract[r"$type"]:
+                if "location" not in final_state:
+                    final_state["location"] = {}
+                final_state["location"][extract.get("SlotIndex", 3)] = {
+                    "player_1_cards": extract["Player1Cards"],
+                    "player_2_cards": extract["Player2Cards"],
                 }
                 pass
             else:
@@ -79,19 +85,36 @@ class LogParser:
         """To-Do."""
         pass
 
-    def extract_ids(self, state:any, result=None):
+    def extract_parent_ids(self, state: any, result=None):
         """Recursively extracts ID objects from game state."""
         if result is None:
             result = []
 
         if isinstance(state, dict):
-            if r'$id' in state:
-                if (r'$type' in state and state[r'$type'] in self.parent_types):
+            if r"$id" in state:
+                if r"$type" in state and state[r"$type"] in self.parent_types:
                     result.append(state)
             for _key, value in state.items():
-                self.extract_ids(value, result)
+                self.extract_parent_ids(value, result)
         elif isinstance(state, list):
             for x in state:
-                self.extract_ids(x, result)
+                self.extract_parent_ids(x, result)
 
         return result
+
+    def extract_specific_ids(self, state: any, obj_id, obj_type, result=None):
+        """Recursively extracts ID objects from game state."""
+        if isinstance(state, dict):
+            if r"$id" in state:
+                # print(state)
+                if r"$type" in state and state[r"$type"] == obj_type and state["$id"] == obj_id:
+                    return state
+            for _key, value in state.items():
+                result = self.extract_specific_ids(value, obj_id, obj_type)
+                if result is not None:
+                    return result
+        elif isinstance(state, list):
+            for x in state:
+                result = self.extract_specific_ids(x, obj_id, obj_type)
+                if result is not None:
+                    return result
